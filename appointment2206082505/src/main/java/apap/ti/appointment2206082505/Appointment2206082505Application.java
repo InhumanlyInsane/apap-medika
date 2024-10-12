5d26921c839dc1c73038de5c506a5bac855d313b
Vincent Suhardi <vinjupt@gmail.com>
2024-10-12 19:27:44 +0700

    initial commit

---
+++ appointment2206082505/src/main/java/apap/ti/appointment2206082505/Appointment2206082505Application.java
@@ -0,0 +1,13 @@
+package apap.ti.appointment2206082505;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Appointment2206082505Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Appointment2206082505Application.class, args);
+	}
+
+}
---
+++ appointment2206082505/src/test/java/apap/ti/appointment2206082505/Appointment2206082505ApplicationTests.java
@@ -0,0 +1,13 @@
+package apap.ti.appointment2206082505;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Appointment2206082505ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

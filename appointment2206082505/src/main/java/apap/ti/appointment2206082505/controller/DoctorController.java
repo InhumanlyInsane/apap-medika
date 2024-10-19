906a2f247c359275b634816947fe86b84ac8a065
Vincent Suhardi <vinjupt@gmail.com>
2024-10-19 20:18:10 +0700

    feat: detail doctor

--- appointment2206082505/src/main/java/apap/ti/appointment2206082505/controller/DoctorController.java
+++ appointment2206082505/src/main/java/apap/ti/appointment2206082505/controller/DoctorController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import apap.ti.appointment2206082505.service.DoctorService;
@@ -23,5 +24,16 @@ public class DoctorController {
         model.addAttribute("doctorService", doctorService);
         model.addAttribute("listDoctor", listDoctor);
         return "all-doctor";
+    }
+
+    @GetMapping("/doctor/{id}")
+    public String detailDoctor(@PathVariable ("id") String id, Model model) {
+        var doctor = doctorService.getDoctorById(id);
+        String schedule = doctorService.convertScheduleToDayNames(doctor.getSchedule());
+
+        model.addAttribute("specializationMap", doctorService.getSpecializationMap());
+        model.addAttribute("schedule", schedule);
+        model.addAttribute("doctor", doctor);
+        return "detail-doctor";
     }
 }
